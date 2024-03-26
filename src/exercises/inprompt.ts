0b0d83e155acaf51d5d6b43f628d12537f644ea4
m.ziolkowski <mziolkowski@kenbit.pl>
2024-03-26 15:24:40 +0100

    Poprawiono zadanie inprompt

--- src/exercises/inprompt.ts
+++ src/exercises/inprompt.ts
@@ -2,66 +2,42 @@ import {answer, getTask} from "../utils/utils.ts";
 import {ChatOpenAI} from "@langchain/openai";
 import {ChatPromptTemplate} from "langchain/prompts";
 
-
 const taskData = await getTask("inprompt");
 const {input, question} = taskData;
-// console.log(input)
-
-// @ts-ignore
-const json = input.map(text => {
-    let name = text.split(" ");
-   return {
-       pageContnt: text,
-       metadata: {
-           name: name[0],
-       }
-   };
-});
-console.log(json)
 
 const chat = new ChatOpenAI({
     openAIApiKey: process.env.OPENAI_API_KEY,
     modelName: "gpt-3.5-turbo",
     maxConcurrency: 5
 });
+
 const match = question.match(/(\w+)\s*\?$/);
 let nameFromQuestion = match ? match[1] : '';
-// console.log(nameFromQuestion)
 
-const systemTemplate = "On entry, you will receive a file in which the {pageContent} contains information about the person's eye colour, hair, occupation and breakfast. \n" +
-    "In {name} is the person's first name. \n" +
-    "\n" +
-    "Your task is to answer the question for the name that is in this {question}\n" +
-    "\n" +
-    "``example\n" +
-    "pageContent: \"Kordian has brown eyes, short hair and works as an architect and likes to eat sausages for breakfast the most.\"\n" +
-    "\n" +
-    "question: `Who is Kordian by profession'.\n" +
-    "answer: \"Architect\"\n" +
-    "question: \"What does Kordian eat for breakfast?\"\n" +
-    "answer: \"sausages\"\n" +
-    "question: \"What colour eyes does Kordian have?\"\n" +
-    "answer: \"brown\"\n" +
-    "```"
+const systemTemplate = `
+Using CONTEXT answer the QUESTION. Answer as short as possible and don't give any explanation. Example: "Blue"
 
-const humanTemplate = "Korzystajac z {pageContent} wyciagnij informacje o które jesteś proszony w pytaniu: {question}"
+### CONTEXT ###
+{context}
+
+### QUESTION ###
+{question}
+`;
+const humanTemplate = "{question}"
 
 const chatPrompt = ChatPromptTemplate.fromMessages([
     ["system", systemTemplate],
     ["human", humanTemplate]
 ]);
-// console.log(chatPrompt)
+
+// @ts-ignore
+const context = input.filter((element) => element.includes(nameFromQuestion)).join(" ");
 
 const formattedChatPrompt = await chatPrompt.formatMessages({
     question: question,
-    pageContent: json.pageContent,
-    name: nameFromQuestion,
+    context: context,
 });
-// console.log(formattedChatPrompt)
-//
+
 // @ts-ignore
 const {content} = await chat.invoke(formattedChatPrompt);
-console.log("content:" + content)
-//
 answer(content)
-
