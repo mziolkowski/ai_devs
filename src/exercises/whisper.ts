7a29aebc174e305fc1fec89da5740dfb309beeac
m.ziolkowski <mziolkowski@kenbit.pl>
2024-03-28 11:33:19 +0100

    refactoring

--- src/exercises/whisper.ts
+++ src/exercises/whisper.ts
@@ -1,7 +1,6 @@
 import {answer, getTask} from "../utils/utils.ts";
 // @ts-ignore
 import {OpenAIWhisperAudio} from "langchain/document_loaders/fs/openai_whisper_audio";
-import OpenAI from "openai";
 import * as fs from "fs";
 import fetch from 'node-fetch';
 import stream from 'stream';
@@ -18,17 +17,10 @@ const response = await fetch(url);
 if (!response.ok) throw new Error(`unexpected response ${response.statusText}`);
 await pipeline(response.body, fs.createWriteStream('mateusz.mp3'));
 
-// Pierwszy sposób wykorzystujący model OpenAiWhisperAudio z pakietu langchain
-// const loader = new OpenAIWhisperAudio("mateusz.mp3", {clientOptions: {apiKey: process.env.OPENAI_API_KEY}})
-// const doc = await loader.load()
-//
-// answer(doc[0].pageContent)
 
-// Drugi sposób wykorzystujący model OpenAI zamiast OpenAiWhisperAudio
-const openai = new OpenAI({apiKey: process.env.OPENAI_API_KEY});
-const transcription = await openai.audio.transcriptions.create({
-    file: fs.createReadStream("mateusz.mp3"),
-    model: "whisper-1"
-});
-answer(transcription.text)
+const loader = new OpenAIWhisperAudio("mateusz.mp3", {clientOptions: {apiKey: process.env.OPENAI_API_KEY}})
+const doc = await loader.load()
+
+answer(doc[0].pageContent)
+
 
